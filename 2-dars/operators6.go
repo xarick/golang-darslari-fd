95cc1e3b94d018b47af14f4f081cf7dceae363ce
Xarick <tokhirovbob@gmail.com>
2022-09-07 11:18:11 +0500

    Initial commit

---
+++ 2-dars/operators6.go
@@ -0,0 +1,15 @@
+// misc (boshqa) operatorlar
+
+package main
+
+import "fmt"
+
+func operators6() {
+	a := 4
+
+	// xotiradagi manzilini ishlatish (&) va ko'rsatkich (*) operatorlari
+	b := &a
+	fmt.Println(*b)
+	*b = 7
+	fmt.Println(a)
+}
